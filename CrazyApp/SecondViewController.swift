167d5bf789d5ba2c721b303188eb02681eb4f380
Sooi De Pauw <sooi@waylay.io>
2021-01-04 18:38:30 +0100

    init commit

---
+++ CrazyApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CrazyApp
+//
+//  Created by Sooi De Pauw on 04/01/2021.
+//  Copyright © 2021 Sooi De Pauw. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
